2581f3bbf9ed9c5cb44b64c5ee05282823eb480a
Iivari Leinonen <iivari.leinonen@gmail.com>
2025-05-13 19:00:35 +0300

    some new goodies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+/**
+ * @type {import('astro').AstroUserConfig}
+ */
 // https://astro.build/config
 export default defineConfig({
+  // Add experimental options here at the same level as integrations
+  experimental: {
+    responsiveImages: true,
+  },
   integrations: [
     tailwind({
       // Configure the Tailwind integration with the new PostCSS plugin
